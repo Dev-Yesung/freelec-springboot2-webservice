5994438bd752a09627ffc669329c47a8e634e8d5
ysng_ysng <yeasung67@gmail.com>
2023-05-01 18:40:03 +0900

    Implement simple web board with OAuth2 login

---
+++ src/main/java/com/yosong/book/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,19 @@
+package com.yosong.book.springboot.web.dto;
+
+import com.yosong.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private final Long id;
+    private final String title;
+    private final String content;
+    private final String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
